ff7bb3095e0bb46f555c22209e1eaa321d0666d3
shreya <shreyarao497@gmail.com>
2025-10-04 21:49:55 +0530

    menu and contact

--- src/app.jsx
+++ src/app.jsx
@@ -8,6 +8,8 @@ import Hero from "./components/Hero.jsx";
 import Cocktails from "./components/Cocktails.jsx";
 import About from "./components/About.jsx";
 import Art from "./components/Art.jsx";
+import Menu from "./components/Menu.jsx";
+import Contact from "./components/Contact.jsx";
 
 const App = () => {
     return (
@@ -18,6 +20,8 @@ const App = () => {
             <Cocktails/>
             <About/>
             <Art/>
+            <Menu/>
+            <Contact/>
         </main>
         </>
     )
