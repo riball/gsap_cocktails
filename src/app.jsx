c9ab48dc605fad66146d22f5dcaffe0e4f73a2e9
shreya <shreyarao497@gmail.com>
2025-10-04 19:28:44 +0530

    about section

--- src/app.jsx
+++ src/app.jsx
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar.jsx'
 import './index.css'
 import Hero from "./components/Hero.jsx";
 import Cocktails from "./components/Cocktails.jsx";
+import About from "./components/About.jsx";
 
 const App = () => {
     return (
@@ -14,6 +15,7 @@ const App = () => {
             <Navbar/>
             <Hero/>
             <Cocktails/>
+            <About/>
         </main>
         </>
     )
