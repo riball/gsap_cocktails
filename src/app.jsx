a881c3a32fd2a11a34625cde9475504acd32b90a
shreya <shreyarao497@gmail.com>
2025-10-04 20:33:00 +0530

    art section

--- src/app.jsx
+++ src/app.jsx
@@ -7,6 +7,7 @@ import './index.css'
 import Hero from "./components/Hero.jsx";
 import Cocktails from "./components/Cocktails.jsx";
 import About from "./components/About.jsx";
+import Art from "./components/Art.jsx";
 
 const App = () => {
     return (
@@ -16,6 +17,7 @@ const App = () => {
             <Hero/>
             <Cocktails/>
             <About/>
+            <Art/>
         </main>
         </>
     )
